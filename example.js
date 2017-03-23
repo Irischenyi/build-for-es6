2024056ab57e605d982c7d93812cb1525d6dc38e
Irischenyi <chenyi4as@163.com>
2017-03-23 16:45:00 +0800

    add all

---
+++ example.js
@@ -0,0 +1,22 @@
+// 'use strict';
+import 'babel-polyfill';
+function* fib(max) {
+    var
+        t,
+        a = 0,
+        b = 1,
+        n = 1;
+    while (n < max) {
+        yield a;
+        t = a + b;
+        a = b;
+        b = t;
+        n ++;
+    }
+    return a;
+}
+
+var f = fib(5);
+f.next();
+f.next();
+f.next();
